92ad52674cd67dbe91e68a2a7735af16904ff3d5
Dawn Wi <wi825@naver.com>
2022-07-05 21:05:56 +0900

    splash(ing)

--- app/src/main/java/com/help_kiosk/MainActivity.java
+++ app/src/main/java/com/help_kiosk/MainActivity.java
@@ -1,6 +1,5 @@
 package com.help_kiosk;
-
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/help_kiosk/SplashFragment.java
@@ -0,0 +1,36 @@
+package com.help_kiosk;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SplashFragment extends Fragment {
+
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+    public static SplashFragment newInstance(String param1, String param2) {
+        SplashFragment fragment = new SplashFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+}
