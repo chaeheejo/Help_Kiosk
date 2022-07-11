2d8485d4cc443169d52a02536150d7a2b51b3351
Dawn Wi <wi825@naver.com>
2022-07-11 20:04:58 +0900

    data structure

---
+++ app/src/main/java/com/help_kiosk/WayViewModel.java
@@ -0,0 +1,9 @@
+package com.help_kiosk;
+
+import androidx.lifecycle.ViewModel;
+
+
+public class WayViewModel extends ViewModel {
+    private UserRepository userRepository = UserRepository.getInstance();
+
+}
