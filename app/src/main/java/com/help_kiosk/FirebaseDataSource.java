e8789bb31e900425e6bdfecbeb30594ddb2cee01
Dawn Wi <wi825@naver.com>
2022-07-11 20:38:24 +0900

    storage (ing)

--- app/src/main/java/com/help_kiosk/FirebaseDataSource.java
+++ app/src/main/java/com/help_kiosk/FirebaseDataSource.java
@@ -1,4 +1,24 @@
 package com.help_kiosk;
 
+import android.widget.Toast;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class FirebaseDataSource {
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference();
+
+    public void getPhoto(DataSourceCallback<Result> callback){
+        StorageReference pathReference = storageRef.child("KakaoTalk_20220706_214041555.jpg");
+        if(pathReference == null){
+            callback.onComplete(new Result.Error(new Exception("Failed")));
+        }else{
+            callback.onComplete(new Result.Success<String>("Success"));
+        }
+    }
+
+    public interface DataSourceCallback<Result>{
+        void onComplete(Result result);
+    }
 }
--- app/src/main/java/com/help_kiosk/WayViewModel.java
+++ app/src/main/java/com/help_kiosk/WayViewModel.java
@@ -6,4 +6,11 @@ import androidx.lifecycle.ViewModel;
 public class WayViewModel extends ViewModel {
     private UserRepository userRepository = UserRepository.getInstance();
 
+    public void getPhoto(){
+        userRepository.getPhoto(result -> {
+            if(result instanceof Result.Success){
+
+            }
+        });
+    }
 }
